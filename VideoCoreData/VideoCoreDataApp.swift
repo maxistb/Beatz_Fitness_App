ccbc6b16354194dda5dcbd9363959fe199966012
maxistb <riel-gemuetlich-09@icloud.com>
2023-04-05 12:16:01 +0200

    Initial Commit

---
+++ VideoCoreData/VideoCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCoreDataApp.swift
+//  VideoCoreData
+//
+//  Created by Maximillian Stabe on 05.04.23.
+//
+
+import SwiftUI
+
+@main
+struct VideoCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
