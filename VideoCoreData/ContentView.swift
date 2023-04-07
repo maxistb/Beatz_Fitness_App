da8365028464aafe09c85edbf4d6d9de033db132
maxistb <riel-gemuetlich-09@icloud.com>
2023-04-07 11:06:41 +0200

    Speichern von Splits und Übungen funktioniert, UI muss noch angepasst werden

--- VideoCoreData/ContentView.swift
+++ VideoCoreData/ContentView.swift
@@ -6,36 +6,79 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
     
-    @State private var splitsName = ""
-    @StateObject var splitsVM = SplitViewModel()
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var splits: FetchedResults<Split>
+    @State private var name = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(splits) {split in
+                        NavigationLink(destination: SplitDetail(split: split)) {
+                            Text(split.name ?? "Error")
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                HStack {
+                    TextField("Add Split", text: $name)
+                        .padding(.leading)
+                    Button("Add") {
+                        let neuerSplit = Split(context: moc)
+                        neuerSplit.id = UUID()
+                        neuerSplit.name = name
+                        
+                        try? moc.save()
+                        name = ""
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+    func deleteItems(at offsets: IndexSet) {
+        for offset in offsets {
+            let split = splits[offset]
+            moc.delete(split)
+        }
+        try? moc.save()
+    }
+}
+
+struct SplitDetail: View {
+    @Environment(\.managedObjectContext) var moc
+    @StateObject var split: Split
+    @State private var name = ""
     
     var body: some View {
         VStack {
-            List {
-                ForEach(splitsVM.splits) { split in
-                    NavigationLink(destination: ÜbungsView(split: split)) {
-                        Text(split.name ?? "Error")
-                    }
+            List(split.getUebungen) {uebungen in
+                HStack {
+                    Text(uebungen.name ?? "Error")
                 }
-                .onDelete(perform: deleteSplit)
             }
             
             HStack {
-                TextField("Neuer Split", text: $splitsName)
-                Button("Hinzufügen") {
-                    splitsVM.addData(name: splitsName)
-                    splitsName = ""
+                TextField("Add Uebung", text: $name)
+                    .padding(.leading)
+                Button("Add") {
+                    let neueUebung = Uebung(context: moc)
+                    neueUebung.id = UUID()
+                    neueUebung.name = name
+                    split.addToUebung(neueUebung)
+                    
+                    try? moc.save()
+                    name = ""
                 }
+                .padding(.trailing)
             }
+            .padding()
         }
-        .padding()
-    }
-    
-    func deleteSplit(at offsets: IndexSet) {
-        splitsVM.delete(offsets: offsets)
     }
 }
 
