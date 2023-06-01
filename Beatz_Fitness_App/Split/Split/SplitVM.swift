d96b14b92ede9665cd52f2738d4ee0b2f2de09f5
maxistb <riel-gemuetlich-09@icloud.com>
2023-06-01 09:50:24 +0200

    MoveItems und DeleteItems funktionieren

--- Beatz_Fitness_App/Split/Split/SplitVM.swift
+++ Beatz_Fitness_App/Split/Split/SplitVM.swift
@@ -16,62 +16,60 @@ extension SplitView {
         try? moc.save()
         
     }
-
+    
     func moveItems(from source: IndexSet, to destination: Int) {
         var revisedSplits: [Split] = splits.map { $0 }
         revisedSplits.move(fromOffsets: source, toOffset: destination)
         
-        // Update the order property of the splits based on their new positions
         for index in 0..<revisedSplits.count {
             revisedSplits[index].order = Int64(index)
         }
         
-        // Save the changes to the managed object context
         for split in revisedSplits {
-            moc.performAndWait {
-                split.order = split.order // Trigger change notification
-            }
+            split.order = split.order
+            print("\(split.name) \(split.order)")
+            
         }
         try? moc.save()
     }
-
     
-//    func moveItems(at sets: IndexSet, destination: Int) {
-//        let itemToMove = sets.first!
-//
-//        if itemToMove < destination {
-//            var startIndex = itemToMove + 1
-//            let endIndex = destination - 1
-//            var startOrder = splits[itemToMove].order
-//            while startIndex <= endIndex {
-//                splits[startIndex].order = startOrder
-//                startOrder += 1
-//                startIndex += 1
-//            }
-//            splits[itemToMove].order = startOrder
-//        }
-//        else if destination < itemToMove {
-//            var startIndex = destination
-//            let endIndex = itemToMove - 1
-//            var startOrder = splits[destination].order + 1
-//            let newOrder = splits[destination].order
-//            while startIndex <= endIndex {
-//                splits[startIndex].order = startOrder
-//                startOrder += 1
-//                startIndex += 1
-//            }
-//            splits[itemToMove].order = newOrder
-//        }
-//        do {
-//            try moc.save()
-//        }
-//        catch {
-//            print(error.localizedDescription)
-//        }
-//    }
     
-
-
+    //    func moveItems(at sets: IndexSet, destination: Int) {
+    //        let itemToMove = sets.first!
+    //
+    //        if itemToMove < destination {
+    //            var startIndex = itemToMove + 1
+    //            let endIndex = destination - 1
+    //            var startOrder = splits[itemToMove].order
+    //            while startIndex <= endIndex {
+    //                splits[startIndex].order = startOrder
+    //                startOrder += 1
+    //                startIndex += 1
+    //            }
+    //            splits[itemToMove].order = startOrder
+    //        }
+    //        else if destination < itemToMove {
+    //            var startIndex = destination
+    //            let endIndex = itemToMove - 1
+    //            var startOrder = splits[destination].order + 1
+    //            let newOrder = splits[destination].order
+    //            while startIndex <= endIndex {
+    //                splits[startIndex].order = startOrder
+    //                startOrder += 1
+    //                startIndex += 1
+    //            }
+    //            splits[itemToMove].order = newOrder
+    //        }
+    //        do {
+    //            try moc.save()
+    //        }
+    //        catch {
+    //            print(error.localizedDescription)
+    //        }
+    //    }
+    
+    
+    
     func updateSplitName(split: Split, newName: String) {
         moc.performAndWait {
             split.name = newName
